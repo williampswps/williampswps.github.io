91ddd5a1f0715f5be3f86541332db6abe0ca9f4b
williampswps <wps.williamps@gmail.com>
2025-06-03 20:38:48 -0300

    update

--- projeto_titanic/js_titanic/dados.js
+++ projeto_titanic/js_titanic/dados.js
@@ -1,7 +1,7 @@
 // Carrega os dados JSON do Titanic de forma assíncrona
 // dados.js
 async function carregarDados() {
-    const resp = await fetch("data/train.json");
+    const resp = await fetch("data_titanic/train.json");
     const dados = await resp.json();
     return dados;
 }
